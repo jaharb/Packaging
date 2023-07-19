44322da89aaed473bf7dea1837d1ea6379b3be8c
jaharb <andy.harbert@francistuttle.edu>
2023-07-19 13:42:59 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import mypkg.MyStack;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        MyStack s = new MyStack();
+        s.print();
+    }
+}
---
+++ src/mypkg/MyStack.java
@@ -0,0 +1,8 @@
+package mypkg;
+
+public class MyStack {
+    public void print()
+    {
+        System.out.println("hello from MyStack");
+    }
+}
